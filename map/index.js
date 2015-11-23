47add19f93b829a2c17693ab1f9074cd7cd52f90
segmentationfaulter <segmentationfaulter@gmail.com>
2015-11-23 21:42:10 +0500

    remove commented code

--- map/index.js
+++ map/index.js
@@ -2,7 +2,6 @@
 function map(arr, callback, context) {
   const mapped = [];
   for (let i = 0; i < arr.length; i++) {
-    // mapped.push(callback(arr[i], i, arr));
     mapped.push(callback.call(context, arr[i], i, arr));
   }
   return mapped;
